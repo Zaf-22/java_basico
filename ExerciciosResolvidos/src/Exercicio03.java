48506e2fe8ed76c3a55dbeb97c30fa377d09cc49
augusto_f_gomes <pretozaf@gmail.com>
2026-06-11 21:08:40 -0300

    Exercício Basico Feito

---
+++ ExerciciosResolvidos/src/Exercicio03.java
@@ -0,0 +1,16 @@
+public class Exercicio03 {
+    public static void main(String[] args) {
+        // 1. Criação do vetor de 5 posições usando 'new int[5]'
+        int[] quadrados = new int[5];
+
+        // 2. Laço 'for' para preencher o vetor com o quadrado do índice
+        for (int i = 0; i < quadrados.length; i++) {
+            quadrados[i] = i * i; // Eleva o índice atual ao quadrado
+        }
+
+        // 3. Laço 'for' para imprimir os valores do vetor
+        for (int i = 0; i < quadrados.length; i++) {
+            System.out.println("posicao " + i + " = " + quadrados[i]);
+        }
+    }
+}
