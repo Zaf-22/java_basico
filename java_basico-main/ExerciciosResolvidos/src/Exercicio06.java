4e51ddd267cf2cd67254f709e971abb704e8a5c9
augusto_f_gomes <pretozaf@gmail.com>
2026-06-18 20:42:15 -0300

    exercicio4

---
+++ java_basico-main/ExerciciosResolvidos/src/Exercicio06.java
@@ -0,0 +1,9 @@
+public class Exercicio06 {
+    public static void main(String[] args) {
+        int[]reverso= {10, 20, 30, 40, 50};
+        for(int i =reverso.length -1; i>=0; i--){
+            System.out.println(reverso[i]);
+
+        }
+    }
+}
